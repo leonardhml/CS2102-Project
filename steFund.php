a81a9f923d4cf3f153cf023dc29ef4bf96d478e5
Feixue <feixuehuang@yahoo.com>
2015-09-28 16:32:22 +0800

    saved

--- steFund.php
+++ steFund.php
@@ -1,8 +1,32 @@
 <html>
 
 <head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
     <title>SteFund - Your one-stop shop for all things crowdfunding!</title>
-    <link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/business-casual.css" rel="stylesheet">
+
+    <!-- Fonts -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
 </head>
 
 <?php
@@ -10,72 +34,207 @@ include 'config.php';
 ?>
 
 <body>
-<div class="header">
-    <h1 id="title">SteFund inc.</h1>
-    <h2 id="subtitle">Please Give Us Money</h2>
-</div>
-<div class="menu">
-    <a href="updateUser.php">Update users</a><br>
-    <a href="insert.php">Insert a user</a>
+    <div class="brand">SteFund inc.</div>
+    <div class="address-bar">Please Give Us Money</div>
 
-</div>
-<table>
-    <thead>
-    <tr>
-        <th colspan="2">Available projects</th>
-    </tr>
-    </thead>
+    <div class="menu">
+        <a href="updateUser.php">Update users</a><br>
+        <a href="insert.php">Insert a user</a>
+    </div>
 
-    <tr>
-        <td id="form" >
-            <form>
-                Title: <input type="text" name="Title" id="formTitle">
-                <input type="submit" name="formSubmit" value="Search" >
-            </form>
-            <?php
-            if(isset($_GET['formSubmit']))
-            {
-                $sql = "SELECT title, in_charge, start_date, end_date, target, raised, description FROM proposed_project WHERE title like '%".$_GET['Title']."%'";
-                echo "<b>SQL: </b>".$sql."<br><br>";
-                $stid = oci_parse($dbh, $sql);
-                oci_execute($stid, OCI_DEFAULT);
-                echo "<table border=\"1\" >
-            <tr>
-                <th>Title</th>
-                <th>Organisation</th>
-                <th>Start Date</th>
-                <th>End Date</th>
-                <th>Target Goal</th>
-                <th>Raised</th>
-                <th>Description</th>
-            </tr>";
-                while($row = oci_fetch_array($stid)) {
-                    echo "<tr>";
-                    echo "<td>" . $row[0] . "</td>";
-                    echo "<td>" . $row[1] . "</td>";
-                    echo "<td>" . $row[2] . "</td>";
-                    echo "<td>" . $row[3] . "</td>";
-                    echo "<td>" . $row[4] . "</td>";
-                    echo "<td>" . $row[5] . "</td>";
-                    echo "<td>" . $row[6] . "</td>";
-                    echo "</tr>";
+    <nav class="navbar navbar-default" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->
+                <a class="navbar-brand" href="index.html">SteFund inc.</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="index.html">Home</a>
+                    </li>
+                    <li>
+                        <a href="about.html">About</a>
+                    </li>
+                    <li>
+                        <a href="blog.html">Projects</a>
+                    </li>
+                    <li>
+                        <a href="contact.html">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+        <div class="container">
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12 text-center">
+                    <div id="carousel-example-generic" class="carousel slide">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators hidden-xs">
+                            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                        </ol>
+
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner">
+                            <div class="item active">
+                                <img class="img-responsive img-full" src="img/slide-1.jpg" alt="">
+                            </div>
+                            <div class="item">
+                                <img class="img-responsive img-full" src="img/slide-2.jpg" alt="">
+                            </div>
+                            <div class="item">
+                                <img class="img-responsive img-full" src="img/slide-3.jpg" alt="">
+                            </div>
+                        </div>
+
+                        <!-- Controls -->
+                        <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                            <span class="icon-prev"></span>
+                        </a>
+                        <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                            <span class="icon-next"></span>
+                        </a>
+                    </div>
+                    <h2 class="brand-before">
+                        <small>Welcome to</small>
+                    </h2>
+                    <h1 class="brand-name">Business Casual</h1>
+                    <hr class="tagline-divider">
+                    <h2>
+                        <small>By
+                            <strong>Start Bootstrap</strong>
+                        </small>
+                    </h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">Build a website
+                        <strong>worth visiting</strong>
+                    </h2>
+                    <hr>
+                    <img class="img-responsive img-border img-left" src="img/intro-pic.jpg" alt="">
+                    <hr class="visible-xs">
+                    <p>The boxes used in this template are nested inbetween a normal Bootstrap row and the start of your column layout. The boxes will be full-width boxes, so if you want to make them smaller then you will need to customize.</p>
+                    <p>A huge thanks to <a href="http://join.deathtothestockphoto.com/" target="_blank">Death to the Stock Photo</a> for allowing us to use the beautiful photos that make this template really come to life. When using this template, make sure your photos are decent. Also make sure that the file size on your photos is kept to a minumum to keep load times to a minimum.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc placerat diam quis nisl vestibulum dignissim. In hac habitasse platea dictumst. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">Beautiful boxes
+                        <strong>to showcase your content</strong>
+                    </h2>
+                    <hr>
+                    <p>Use as many boxes as you like, and put anything you want in them! They are great for just about anything, the sky's the limit!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc placerat diam quis nisl vestibulum dignissim. In hac habitasse platea dictumst. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <p>Copyright &copy; CS2102 Project 2015</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Script to Activate the Carousel -->
+    <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    </script>
+
+    <table>
+        <thead>
+        <tr>
+            <th colspan="2">Available projects</th>
+        </tr>
+        </thead>
+
+        <tr>
+            <td id="form" >
+                <form>
+                    Title: <input type="text" name="Title" id="formTitle">
+                    <input type="submit" name="formSubmit" value="Search" >
+                </form>
+                <?php
+                if(isset($_GET['formSubmit']))
+                {
+                    $sql = "SELECT title, in_charge, start_date, end_date, target, raised, description FROM proposed_project WHERE title like '%".$_GET['Title']."%'";
+                    echo "<b>SQL: </b>".$sql."<br><br>";
+                    $stid = oci_parse($dbh, $sql);
+                    oci_execute($stid, OCI_DEFAULT);
+                    echo "<table border=\"1\" >
+                <tr>
+                    <th>Title</th>
+                    <th>Organisation</th>
+                    <th>Start Date</th>
+                    <th>End Date</th>
+                    <th>Target Goal</th>
+                    <th>Raised</th>
+                    <th>Description</th>
+                </tr>";
+                    while($row = oci_fetch_array($stid)) {
+                        echo "<tr>";
+                        echo "<td>" . $row[0] . "</td>";
+                        echo "<td>" . $row[1] . "</td>";
+                        echo "<td>" . $row[2] . "</td>";
+                        echo "<td>" . $row[3] . "</td>";
+                        echo "<td>" . $row[4] . "</td>";
+                        echo "<td>" . $row[5] . "</td>";
+                        echo "<td>" . $row[6] . "</td>";
+                        echo "</tr>";
+                    }
+                    echo "</table>";
+                    oci_free_statement($stid);
                 }
-                echo "</table>";
-                oci_free_statement($stid);
-            }
-            ?>
-        </td>
-    </tr>
+                ?>
+            </td>
+        </tr>
 
-    <?php
-    oci_close($dbh);
-    ?>
+        <?php
+        oci_close($dbh);
+        ?>
 
-    <tr>
-        <td colspan="2" style="background-color:#FFA500; text-align:center;">
-            Copyright &#169; CS2102
-        </td>
-    </tr>
-</table>
+    </table>
+    
 </body>
 </html>
